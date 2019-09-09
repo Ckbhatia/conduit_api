114b5c8b547bc7bdc634ba971e59c6195cdf7058
Ckbhatia <chetansain86@gmail.com>
2019-09-09 22:21:48 +0530

    feat: add users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Post registeration user */
+router.post('/', (req, res, next) => {
+  res.send('Got registeration req');
+});
+
+/* POST login user. */
+router.post('/login', (req, res, next) => {
+  const data = req.body;
+  // Temp
+  res.send(data);
 });
 
 module.exports = router;
