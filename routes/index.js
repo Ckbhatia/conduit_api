2fc6cb01be4acbacd3e4eb0ac817609f3bc95a08
Ckbhatia <chetansain86@gmail.com>
2019-09-10 10:49:36 +0530

    feat: add root and tags route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // TODO
+  // GET /
+  // Expose the API endpoints
+  res.send('Expose the API endpoints');
 });
+
+// Get Tags
+router.get('/api/tags', (req, res, next) => {
+  // TODO
+  // GET /api/tags
+  // No authentication required, returns a List of Tags
+  res.send('Tags are gathered');
+});
+
 
 module.exports = router;
