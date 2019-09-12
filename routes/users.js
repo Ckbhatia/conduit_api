1d61d9c4d18a60b33632c55c8c0b4817bad73155
Ckbhatia <chetansain86@gmail.com>
2019-09-12 20:52:59 +0530

    feat: add user registeration and login

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Users = require('../models/users');
+const Auth = require('../auth/auth');
 
 /* Post registeration user */
 router.post('/', (req, res, next) => {
-  res.send('Got registeration req');
+  Users.create(req.body, (err, user) => {
+    if(err) return next(err);
+    res.json(user);
+  });
 });
 
 /* POST login user. */
-router.post('/login', (req, res, next) => {
-  const data = req.body;
-  // Temp
-  res.send(data);
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  Users.findOne({email}, async (err, user) => {
+    if (err) return res.json({msg: err});
+    if(!user) return res.status(400).json({status:'failed', message:'Invaild credentials'})
+    if(!user.verifyPassword(password)) {
+      return res.status(400).json({status:'failed', message:'Invaild password'});
+    };
+    let token = await Auth.genToken(user.id);
+    res.status(200).json({status:'success',message:'user logged in',token});
+  });
 });
 
 module.exports = router;
