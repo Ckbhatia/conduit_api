f5b092fc90217ab7278925aadc032e4b91bf1e87
Ckbhatia <chetansain86@gmail.com>
2019-09-12 20:38:24 +0530

    feat: add auth JWT functionality

---
+++ auth/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * Generates JWT token
+ * @param {string}
+ * @returns {string}
+ */
+exports.genToken = userId => {
+    return jwt.sign({userId}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+};
+
+/**
+ * Verifies the token
+ * @param {object}
+ * @param {object}
+ * @param {function}
+ * @returns {object}
+ */
+exports.verToken = (req, res, next) => {
+    let token = req.headers.Authorization || '';
+    if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err,user) => {
+        if(err) return res.status(401).json({message:'invalid token'});
+        req.userId = user.userId;
+        next();
+    });
+    }
+    else res.status(401).json({message:'unauthorized access'});
+};
