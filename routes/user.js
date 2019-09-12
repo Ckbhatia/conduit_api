d124c8cad87f46270d44a35f07ab3b7b6b7dfe9c
Ckbhatia <chetansain86@gmail.com>
2019-09-12 22:04:32 +0530

    feat: add get and update current user functionality

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Auth = require('../auth/auth');
+const Users = require('../models/users');
+
+// Check authorization first
+router.use(Auth.verToken);
 
 /* Get current user */
-router.get('/', (req, res, next) => {
-  res.send('Got req for current user');
+router.get('/', (req, res) => {
+  const userId = req.userId;
+  Users.findById(userId, (err, user) => {
+    if(err) return res.json({msg: err});
+    if(!user) return res.json({msg: 'User not found'});
+    res.json({user});
+  });
 });
 
 /* POST login user. */
-router.put('/', (req, res, next) => {
+router.put('/', (req, res) => {
   const data = req.body;
-  // Temp
-  res.send(data);
-//   Todo
-// Add User modal and update functionality
+  const userId = req.userId;
+  Users.findOneAndUpdate(userId, data,{new:true} ,(err, user) => {
+    if (err) return res.json({msg: err});
+    user.password = data.password;
+    user.save((err, updateduser) => {
+      if (err) return res.json({msg: err});
+      res.json({updateduser});
+    });
+  });
 });
 
 module.exports = router;
