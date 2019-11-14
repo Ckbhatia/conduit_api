1013ea9e17fa9a2aa01775b81ed1b1877a13d391
Ckbhatia <chetansain86@gmail.com>
2019-11-14 18:41:23 +0530

    fix: add user to request object

--- auth/auth.js
+++ auth/auth.js
@@ -1,12 +1,13 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const Users = require("../models/users");
 
 /**
  * Generates JWT token
  * @param {string}
  * @returns {string}
  */
-exports.genToken = user => {
-    return jwt.sign({userId: user.id, username: user.username}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+exports.genToken = userId => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "2h" });
 };
 
 /**
@@ -17,15 +18,18 @@ exports.genToken = user => {
  * @returns {object}
  */
 exports.verToken = (req, res, next) => {
-    //Note: First character is lower case for some error reasons, authorization.
-    let token = req.headers.authorization || '';
-    if (token) {
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err,user) => {
-        if(err) return res.status(401).json({message:'invalid token'});
-        req.userId = user.userId;
-        req.username = user.username;
+  //Note: First character is lower case for some error reasons, authorization.
+  let token = req.headers.authorization || "";
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, userToken) => {
+      if (err) return res.status(401).json({ message: "invalid token" });
+      Users.findById(userToken.userId, (err, user) => {
+        if (err) return res.json({ user });
+        if (!user)
+          return res.json({ message: "There's an error. So, user not found" });
+        req.user = user;
         next();
+      });
     });
-    }
-    else res.status(401).json({message:'unauthorized access'});
+  } else res.status(401).json({ message: "unauthorized access" });
 };
