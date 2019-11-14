2e005d5d29d692711d91627121cceb541599cb5e
Ckbhatia <chetansain86@gmail.com>
2019-11-14 14:55:27 +0530

    feat: add find user functionality

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,36 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Users = require("../models/users");
+const Auth = require("../auth/auth");
 
 /* Handle profiles routes */
 
 // Get username
-router.get('/:username', (req, res, next) => {
-    // TODO
-    // Get profile
-    // Authentication optional, returns a Profile
-    res.send('Profile has been sent');
+router.get("/:username", (req, res) => {
+  const username = req.params.username;
+  Users.findOne(
+    { username },
+    "-_id bio image following username",
+    (err, profile) => {
+      if (err) return res.json({ msg: err });
+      if (!profile)
+        return res.status(404).json({ message: "User doesn't exist" });
+      res.json({ profile });
+    }
+  );
 });
 
+// Check authorization first
+router.use(Auth.verToken);
 
 // Get username follow
-router.get('/:username/follow', (req, res, next) => {
-    // TODO
-    // Follow user
-    // Authentication required, returns a Profile
-    // No additional parameters required
-    res.send('Author has been followed');
+router.get("/:username/follow", (req, res) => {
+  // TODO
+  // Follow user
+  // Authentication required, returns a Profile
+  // No additional parameters required
 });
-
 
 // Delete username follow
-router.delete('/:username/follow', (req, res, next) => {
-    // TODO
-    // Unfollow user
-    // Authentication required, returns a Profile
-    // No additional parameters required
-    res.send('Author has been unfollowed');
+router.delete("/:username/follow", (req, res, next) => {
+  // TODO
+  // Unfollow user
+  // Authentication required, returns a Profile
+  // No additional parameters required
+  res.send("Author has been unfollowed");
 });
-
 
 // Export the router
 module.exports = router;
