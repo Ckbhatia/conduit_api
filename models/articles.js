dee581abd6cb5d9e16a13a8c01dae45df7111a8c
Ckbhatia <chetansain86@gmail.com>
2019-11-18 21:54:54 +0530

    fix: replace requirements of the articles schema

--- models/articles.js
+++ models/articles.js
@@ -1,25 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 // Construct articlesSchema
-const articlesSchema = Schema({
-    slug: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type: String, require: true},
-    body: {type: String, required: true},
-    // TODO
-    // Update tags
-    tagsList: String,
-    favorited: Boolean,
+const articlesSchema = Schema(
+  {
+    slug: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, require: true },
+    body: { type: String, required: true },
+    tagsList: [{ type: Schema.Types.ObjectId, ref: "Tags" }],
+    favorited: [{ type: Schema.Types.ObjectId, ref: "Users" }],
     favoritesCount: Number,
-    // TODO
-    // Update author
-    author: {type: String, required: true}
-}, {timestamps: true});
+    author: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
 // Create an instance of Articles model
-const Articles = mongoose.model('Articles', articlesSchema);
+const Articles = mongoose.model("Articles", articlesSchema);
 
 // Exports the articles model
 module.exports = Articles;
