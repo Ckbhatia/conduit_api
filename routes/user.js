21fa80d7566d25c61fc8144cb78c2a3a3caa2cae
Ckbhatia <chetansain86@gmail.com>
2019-11-19 21:17:47 +0530

    fix: replace the userId variable's value

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Auth = require('../auth/auth');
-const Users = require('../models/users');
+const Auth = require("../auth/auth");
+const Users = require("../models/users");
 
 // Check authorization first
 router.use(Auth.verToken);
 
 /* Get current user */
-router.get('/', (req, res) => {
-  const userId = req.userId;
-  Users.findById(userId, (err, user) => {
-    if(err) return res.json({msg: err});
-    if(!user) return res.json({msg: 'User not found'});
-    res.json({user});
-  });
+router.get("/", (req, res) => {
+  const user = req.user;
+  res.json({ user });
 });
 
 /* POST login user. */
-router.put('/', (req, res) => {
+router.put("/", (req, res) => {
   const data = req.body;
-  const userId = req.userId;
-  Users.findOneAndUpdate(userId, data,{new:true} ,(err, user) => {
-    if (err) return res.json({msg: err});
+  const userId = req.user.id;
+  Users.findOneAndUpdate(userId, data, { new: true }, (err, user) => {
+    if (err) return res.json({ msg: err });
     user.password = data.password;
     user.save((err, updateduser) => {
-      if (err) return res.json({msg: err});
-      res.json({updateduser});
+      if (err) return res.json({ msg: err });
+      res.json({ updateduser });
     });
   });
 });
